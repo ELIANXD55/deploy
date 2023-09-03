969980d63a0f1854f7d939005ff111c2dd2e8426
Joan Elian <jevillaurrutia@gmail.com>
2023-09-03 11:40:25 -0500

    a

--- deploy/settings.py
+++ deploy/settings.py
@@ -97,8 +97,7 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(        
-    # Feel free to alter this value to suit your needs.        
+    'default': dj_database_url.config(              
     default='sqlite:/// db.sqlite3',
     onn_max_age=600    
     )}
